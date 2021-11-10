a741c980c72569fb89c42a9f8ad543a34a18b2c5
Chin Lee Wong <chinlee.wong@hotmail.com>
2021-11-10 18:09:21 +0100

    Yelp MVP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,28 +1,30 @@
+
 class RestaurantsController < ApplicationController
   def index
-    @restaurant = Restaurant.all
+    @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def new
-    @restautant = Restaurant.new
+    @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "new"
     end
   end
 
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-  end
+  private
 
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
